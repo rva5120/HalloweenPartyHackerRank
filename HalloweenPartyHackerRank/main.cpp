ce2ae866367201ebcb5e0df55b290200d6c0c076
Raquel Alvarez <rva5120@gmail.com>
2014-12-31 04:30:30 -0600

    Final version

--- HalloweenPartyHackerRank/main.cpp
+++ HalloweenPartyHackerRank/main.cpp
@@ -6,10 +6,37 @@
 //  Copyright (c) 2014 R. All rights reserved.
 //
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    unsigned long long k[10];
+    int T;
+    
+    cin >> T;
+    
+    for (int i = 0; i < T; i++)
+    {
+        cin >> k[i];
+    }
+    
+    
+    for (int j = 0; j < T; j++)
+    {
+        if (k[j] % 2 == 0)
+        {
+            cout << ((k[j]+1)/2)*(k[j]/2) << endl;
+        }
+        else
+        {
+            cout << ((k[j]+1)/2)*(k[j]/2) << endl;
+        }
+    }
     return 0;
 }
